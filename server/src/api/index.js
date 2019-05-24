a691da69bb8435b323635f1f0e2987299cadff5b
Operation Spark <operationspark@Operations-MacBook-Pro-93.local>
2019-05-24 15:41:43 -0500

    feat: Add posts router to api/index.js

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const { users } = require('./users');
+const { posts } = require('./posts');
 
 const api = express.Router();
 
 api.use('/users', users);
+api.use('/posts', posts);
 
 module.exports.api = api;
